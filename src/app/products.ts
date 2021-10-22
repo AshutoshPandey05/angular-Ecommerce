112da19b027239b1609c2eac3b23dc71582aa78a
AshutoshPandey05 <ashunist99@gmail.com>
2021-10-22 10:07:45 +0530

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'IPhone 12 Max Pro',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'Samsung Galaxy S20',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'Motorola G20',
+    price: 299,
+    description: '',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
